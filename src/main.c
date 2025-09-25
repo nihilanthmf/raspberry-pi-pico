197d36a0bb4298bd43a6d95d3dc8d2624d908bd9
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-09-25 22:52:26 +0500

    added a debug message that gets printed to the computer

--- src/main.c
+++ src/main.c
@@ -1,33 +1,24 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
-#endif
-
-// Initialize the GPIO for the LED
-void pico_led_init(void) {
-#ifdef PICO_DEFAULT_LED_PIN
-    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-    // so we can use normal GPIO functionality to turn the led on and off
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-#endif
-}
-
-// Turn the LED on or off
-void pico_set_led(bool led_on) {
-#ifdef PICO_DEFAULT_LED_PIN
-    // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#endif
-}
-
 int main() {
-    pico_led_init();
+    // Initialize stdio (USB or UART depending on build config)
+    stdio_init_all();
+
+    // Set onboard LED pin
+    const uint LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (true) {
-        pico_set_led(true);
-        sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
-        sleep_ms(LED_DELAY_MS);
+        printf("Hello from Raspberry Pi Pico!\n");
+
+        // Turn LED on
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+
+        // Turn LED off
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
     }
 }
