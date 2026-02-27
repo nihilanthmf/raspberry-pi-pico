7d350888da22efb5e9d9e9b7c588cf275949ee8e
Artemy Medvedev <artemiy.logmy@gmail.com>
2026-02-27 11:16:39 +0100

    temperature + humidity sensor code, it works

--- src/sensor.c
+++ src/sensor.c
@@ -5,7 +5,7 @@
 int main() {
     stdio_init_all();
 
-    const uint PIN = 15;
+    const uint PIN = 11;
     gpio_init(PIN);
 
     sleep_ms(4000);
@@ -21,8 +21,8 @@ int main() {
         gpio_pull_up(PIN);
 
         // handling handshake
-        while (gpio_get(PIN) == 0) sleep_us(1);
-        while (gpio_get(PIN) == 1) sleep_us(1);
+        while (gpio_get(PIN) == 0);
+        while (gpio_get(PIN) == 1);
 
         // processing sensor's data
         // every bit starts with 50us LOW
@@ -31,39 +31,55 @@ int main() {
 
         int result[40];
 
-        for (int i = 0; i < 40; ++i) {
-            while (gpio_get(PIN) == 0) sleep_us(1);
+        char current_value = 0;
+        char values[5];
+        int value_index = 0;
 
-            int ones_cnt = 0;
-            int time_before_ones = time_us_32();
-            while (gpio_get(PIN) == 1) {
-                ones_cnt = time_us_32() - time_before_ones;
-                // sleep_us(1);
+        uint32_t status = save_and_disable_interrupts();
+
+        for (int i = 0; i < 40; ++i) {
+            int low_cnt = 0;
+            while (gpio_get(PIN) == 0) {
+
             }
 
-            if (ones_cnt > 60) {
-                // its a 1
+            int start_time = time_us_32();
+            while (gpio_get(PIN) == 1);
+            int high_duration = time_us_32() - start_time;
+
+            if (high_duration > 50) {
                 result[i] = 1;
+
+                current_value <<= 1;
+                current_value |= 1;
             } else {
-                // its a 0
                 result[i] = 0;
+                
+                current_value <<= 1;
+            }
+
+            if ((i+1) % 8 == 0) {
+                values[value_index] = current_value;
+                current_value = 0;
+                ++value_index;
             }
         }
 
-        for (int i = 0; i < 40; ++i) {
-            printf("%d", result[i]);
-        }
-        printf("\n\n");
+        restore_interrupts(status);
 
-        for (int i = 0; i < 40; ++i) {
-            if (i % 8 == 0) {
-                printf("\n");
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                printf("%d", result[(i*8) + j]);
             }
-            printf("%d", result[i]);
+            printf("   %d\n", values[i]);
         }
-        printf("\n\n");
-
-        // restore_interrupts(status);
+        printf("---\nTemperature (C): %d; Humidity: %d\n", values[2], values[0]);
+        if (values[4] == (values[0] +  values[1] +  values[2] +  values[3])) {
+            printf("Correct!\n");
+        } else {
+            printf("!!!! Incorrect !!!!\n");
+        }
+        printf("---\n");
 
         // repeating it with a X ms delay
         sleep_ms(1000);
